8807dc06bf4804773923bddcea1e43f2ee80fe0d
Michael Krotscheck <krotscheck@gmail.com>
2016-03-22 04:24:04 -0700

    Updates from generator-generator
    This patch adds the initial project structure constructed by
    yeoman's project generator.

---
+++ test/app.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+  var path = require('path');
+  var assert = require('yeoman-assert');
+  var helpers = require('yeoman-generator').test;
+
+  describe('generator-openstack:app', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withOptions({someOption: true})
+        .withPrompts({someAnswer: true})
+        .on('end', done);
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'dummyfile.txt'
+      ]);
+    });
+  });
+})();
