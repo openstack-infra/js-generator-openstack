af1209448826cde0b2fcd3cdce3f41f4f73307ca
Michael Krotscheck <krotscheck@gmail.com>
2016-05-16 08:51:00 -0700

    Added project detail prompts.
    These prompts include things like project name, description,
    authors, and homepage, all set with sane defaults.
    
    Change-Id: Ia0f16b517c64953af77dd82829361ab5305f95d8

--- generators/app/lib/component/pkg.js
+++ generators/app/lib/component/pkg.js
@@ -1,19 +1,60 @@
-(function() {
+(function () {
   'use strict';
 
+  var Q = require('q');
   var pkgBuilder = require('../pkg_builder');
   var projectBuilder = require('../project_builder');
 
   var packagePath = 'package.json';
 
   /**
-   * No-op placeholder method, for handlers we don't need.
+   * Iterate over needed package components, ensuring that required
+   * parameters are set.
    *
    * @param {generator} generator The currently active generator.
    * @returns {generator} The passed generator, for promise chaining.
    */
-  function noop (generator) {
-    return generator;
+  function promptPackage (generator) {
+    var deferred = Q.defer();
+
+    if (!generator.options['non-interactive']) {
+      generator.prompt(
+        [{
+          type: 'input',
+          name: 'name',
+          message: 'Project- Name:',
+          default: pkgBuilder.getValue('name')
+        }, {
+          type: 'input',
+          name: 'description',
+          message: 'Project- Description:',
+          default: pkgBuilder.getValue('description')
+        }, {
+          type: 'input',
+          name: 'version',
+          message: 'Project- Version:',
+          default: pkgBuilder.getValue('version', '0.0.1')
+        }, {
+          type: 'input',
+          name: 'homepage',
+          message: 'Project- Homepage:',
+          default: pkgBuilder.getValue('homepage', 'http://www.openstack.org/')
+        }, {
+          type: 'input',
+          name: 'author',
+          message: 'Project- Author:',
+          default: pkgBuilder.getValue('author',
+            'OpenStack <openstack-dev@lists.openstack.org> (http://www.openstack.org/)')
+        }],
+        function (answers) {
+          pkgBuilder.setValues(answers);
+          deferred.resolve(generator);
+        });
+    } else {
+      deferred.resolve(generator);
+    }
+
+    return deferred.promise;
   }
 
   /**
@@ -46,7 +87,7 @@
 
   module.exports = {
     init: initializePackage,
-    prompt: noop,
+    prompt: promptPackage,
     configure: configurePkg
   };
 })();
