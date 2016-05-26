1ee651fd0e23808810757d42bc7273ca871d85ea
Michael Krotscheck <krotscheck@gmail.com>
2016-05-26 10:17:21 -0700

    .gitignore file is now read and echoed back into the project
    This patch allows the generator to read a .gitignore file and
    echo it back into the project. This implementation is destructive
    of the original format, and can be improved in the future.
    
    Change-Id: I38fb29e564120464406c00586952eee0c19150e1

--- generators/app/lib/component/gitignore.js
+++ generators/app/lib/component/gitignore.js
@@ -3,6 +3,9 @@
 
   var projectBuilder = require('../project_builder');
 
+  var excludedPaths = {};
+  var filePath = '.gitignore';
+
   /**
    * No-op placeholder method, for handlers we don't need.
    *
@@ -14,19 +17,55 @@
   }
 
   /**
-   * Configure the project by adding required files.
+   * Read the existing .gitignore file, and populate our current list of ignores.
+   *
+   * @param {generator} generator The currently active generator.
+   * @returns {generator} The passed generator, for promise chaining.
+   */
+  function initGitignore (generator) {
+    var fs = generator.fs;
+
+    // Reinitialize the ignore map.
+    excludedPaths = {};
+
+    // Read .gitignore.
+    if (fs.exists(filePath)) {
+      fs.read(filePath)
+        // Split into lines
+        .split('\n')
+        // Remove empty lines and comments.
+        .filter(function (item) {
+          return item.length > 0 && !item.match(/\s*#/);
+        })
+        // Add to our list.
+        .map(function (line) {
+          excludedPaths[line] = true;
+        });
+    }
+
+    return generator;
+  }
+
+  /**
+   * Generate the .gitignore file from the data we've imported.
    *
    * @param {generator} generator The currently active generator.
    * @returns {generator} The passed generator, for promise chaining.
    */
   function configureGitIgnore (generator) {
-    projectBuilder.addFile('.gitignore');
+    var ignoreContent = Object.keys(excludedPaths).sort().join('\n');
+    if (ignoreContent.length === 0) {
+      // Delete the file if there's nothing to ignore.
+      projectBuilder.removeFile(filePath);
+    } else {
+      projectBuilder.writeFile(filePath, ignoreContent);
+    }
 
     return generator;
   }
 
   module.exports = {
-    init: noop,
+    init: initGitignore,
     prompt: noop,
     configure: configureGitIgnore
   };
