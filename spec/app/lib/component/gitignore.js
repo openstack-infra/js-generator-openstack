7e8feb7b21d9ca0af3933a416f73161a9c71f953
Michael Krotscheck <krotscheck@gmail.com>
2016-05-26 10:17:21 -0700

    .gitignore file is now read and echoed back into the project
    This patch allows the generator to read a .gitignore file and
    echo it back into the project. This implementation is destructive
    of the original format, and can be improved in the future.
    
    Change-Id: I38fb29e564120464406c00586952eee0c19150e1

--- spec/app/lib/component/gitignore.js
+++ spec/app/lib/component/gitignore.js
@@ -28,7 +28,7 @@
           expect(outputGenerator).toEqual(mockGenerator);
         });
 
-      it('should do nothing',
+      it('should not interact with config',
         function () {
           var spy = spyOn(mockGenerator.config, 'defaults');
           gitignore.init(mockGenerator);
@@ -46,6 +46,7 @@
       it('should do nothing',
         function () {
           var spy = spyOn(mockGenerator, 'prompt');
+          gitignore.init(mockGenerator);
           gitignore.prompt(mockGenerator);
           expect(spy.calls.any()).toBeFalsy();
         });
@@ -60,13 +61,112 @@
 
       it('should add gitignore to the project files.',
         function () {
+          mockGenerator.fs.write('.gitignore', ['one', 'two'].join('\n'));
+          gitignore.init(mockGenerator);
+          gitignore.prompt(mockGenerator);
           gitignore.configure(mockGenerator);
 
           var files = projectBuilder.getIncludedFiles();
           expect(files.length).toBe(1);
-          expect(files[0].from).toBe('.gitignore');
           expect(files[0].to).toBe('.gitignore');
         });
+    });
+
+    describe('.gitignore management', function () {
+
+      it('should echo back existing .gitignore',
+        function () {
+          mockGenerator.fs.write('.gitignore', ['one', 'two'].join('\n'));
+
+          gitignore.init(mockGenerator);
+          gitignore.prompt(mockGenerator);
+          gitignore.configure(mockGenerator);
+
+          var files = projectBuilder.getIncludedFiles();
+          var ignoreRef = files[0];
+          var ignoreContent = ignoreRef.content.split('\n');
+          expect(ignoreContent.length).toBe(2);
+
+          expect(ignoreContent[0]).toEqual('one');
+          expect(ignoreContent[1]).toEqual('two');
+        });
+
+      it('should sort the ignored files.',
+        function () {
+          mockGenerator.fs.write('.gitignore', ['b_line', 'a_line'].join('\n'));
+
+          gitignore.init(mockGenerator);
+          gitignore.prompt(mockGenerator);
+          gitignore.configure(mockGenerator);
+
+          var files = projectBuilder.getIncludedFiles();
+          var ignoreRef = files[0];
+          var ignoreContent = ignoreRef.content.split('\n');
+          expect(ignoreContent.length).toBe(2);
+          expect(ignoreContent[0]).toBe('a_line');
+          expect(ignoreContent[1]).toBe('b_line');
+        });
+
+      it('should remove any whitespace from the existing .gitignore',
+        function () {
+          mockGenerator.fs.write('.gitignore', ['1_one', '', '2_two', ''].join('\n'));
+
+          gitignore.init(mockGenerator);
+          gitignore.prompt(mockGenerator);
+          gitignore.configure(mockGenerator);
+
+          var files = projectBuilder.getIncludedFiles();
+          var ignoreRef = files[0];
+          var ignoreContent = ignoreRef.content.split('\n');
+          expect(ignoreContent.length).toBe(2);
+          expect(ignoreContent[0]).toBe('1_one');
+          expect(ignoreContent[1]).toBe('2_two');
+        });
+
+      it('should remove any comments from the existing .gitignore',
+        function () {
+          mockGenerator.fs.write('.gitignore', ['1_one', '# comment', '  #comment'].join('\n'));
+
+          gitignore.init(mockGenerator);
+          gitignore.prompt(mockGenerator);
+          gitignore.configure(mockGenerator);
+
+          var files = projectBuilder.getIncludedFiles();
+          var ignoreRef = files[0];
+          var ignoreContent = ignoreRef.content.split('\n');
+          expect(ignoreContent.length).toBe(1);
+          expect(ignoreContent[0]).toBe('1_one');
+        });
+
+      it('should deduplicate content',
+        function () {
+          mockGenerator.fs.write('.gitignore', ['1_one', '1_one'].join('\n'));
+
+          gitignore.init(mockGenerator);
+          gitignore.prompt(mockGenerator);
+          gitignore.configure(mockGenerator);
+
+          var files = projectBuilder.getIncludedFiles();
+          var ignoreRef = files[0];
+          var ignoreContent = ignoreRef.content.split('\n');
+          expect(ignoreContent.length).toBe(1);
+          expect(ignoreContent[0]).toBe('1_one');
+        });
+
+      it('should delete the file if there\'s nothing to ignore', function () {
+        mockGenerator.fs.write('.gitignore', '');
+
+        gitignore.init(mockGenerator);
+        gitignore.prompt(mockGenerator);
+        gitignore.configure(mockGenerator);
+
+        var files = projectBuilder.getIncludedFiles();
+        expect(files.length).toBe(0);
+
+        var rmFiles = projectBuilder.getExcludedFiles();
+        expect(rmFiles.length).toBe(1);
+        expect(rmFiles[0]).toBe('.gitignore');
+      });
     });
   });
 })();
