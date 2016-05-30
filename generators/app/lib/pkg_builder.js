9a1e234886c7fc8a1d5cdafd8aba1ab426585c6d
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-05-30 23:17:46 +0800

    (WIP) add jsdoc3 to generate api documentation
    run `npm run doc` and serve the document from `docs`
    
    Change-Id: Ica8202a10b1d2c2a35453637f600501a71e412b7
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

--- generators/app/lib/pkg_builder.js
+++ generators/app/lib/pkg_builder.js
@@ -13,154 +13,151 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+'use strict';
 
-(function () {
-  'use strict';
+var dependencies = require('./global_dependencies');
+var pkgContent = {};
 
-  var dependencies = require('./global_dependencies');
-  var pkgContent = {};
+/**
+ * Convert a package.json formatted list of dependencies and update them to the versions
+ * listed in our global dependencies.
+ *
+ * @param {{}} dependencyMap The map of dependencies.
+ * @returns {{}} A clone of the map, updated with current common versions.
+ */
+function synchronizeDependencies (dependencyMap) {
+  if (!dependencyMap) {
+    return undefined;
+  }
 
-  /**
-   * Convert a package.json formatted list of dependencies and update them to the versions
-   * listed in our global dependencies.
-   *
-   * @param {{}} dependencyMap The map of dependencies.
-   * @returns {{}} A clone of the map, updated with current common versions.
-   */
-  function synchronizeDependencies (dependencyMap) {
-    if (!dependencyMap) {
-      return undefined;
+  // Cloning not necessary, handled in dependencies.synchronize();
+  return dependencies.synchronize(dependencyMap);
+}
+
+/**
+ * Initialize this builder from a JSON string.
+ *
+ * @param {String} pkgString The package string content.
+ * @returns {void}
+ */
+function readPackage (pkgString) {
+  pkgContent = JSON.parse(pkgString);
+}
+
+/**
+ * Write the package content to a JSON string.
+ *
+ * @returns {String} The JSON content of the package, as a string.
+ */
+function writePackage () {
+  // Clone the package content so we don't destroy what's in memory...
+  var newContent = JSON.parse(JSON.stringify(pkgContent));
+
+  // Synchronize all the dependencies.
+  newContent.peerDependencies = synchronizeDependencies(newContent.peerDependencies);
+  newContent.dependencies = synchronizeDependencies(newContent.dependencies);
+  newContent.devDependencies = synchronizeDependencies(newContent.devDependencies);
+  return JSON.stringify(newContent, null, 2);
+}
+
+/**
+ * Add libraries to the package dependencies.
+ *
+ * @param {Array|String} libraryNames A list of all libraries to add to the dependencies.
+ * @param {String} type The type of dependency.
+ * @returns {void}
+ */
+function addDependencies (libraryNames, type) {
+  // Default the type.
+  type = type || 'dependencies';
+
+  // Valuecheck type.
+  if (['devDependencies', 'peerDependencies', 'dependencies'].indexOf(type) === -1) {
+    return;
+  }
+
+  // Default the array.
+  if (!Array.isArray(libraryNames)) {
+    libraryNames = [libraryNames];
+  }
+
+  // Make sure the property exists.
+  if (!pkgContent.hasOwnProperty(type)) {
+    pkgContent[type] = {};
+  }
+
+  // Add the dependency
+  libraryNames.forEach(function (library) {
+    var version = dependencies.read(library);
+    if (version && !pkgContent[type].hasOwnProperty(library)) {
+      pkgContent[type][library] = version;
     }
+  });
+}
 
-    // Cloning not necessary, handled in dependencies.synchronize();
-    return dependencies.synchronize(dependencyMap);
+/**
+* Set values on the current package.
+*
+* @param {{}} values A map of values.
+* @returns {void}
+*/
+function setValues (values) {
+for (var key in values) {
+  // Filter out things from prototype.
+  if (values.hasOwnProperty(key)) {
+    pkgContent[key] = values[key];
+  }
+}
+}
+
+/**
+* Get the values of the current package.
+*
+* @returns {{}} A cloned map of the values.
+*/
+function getValues () {
+return JSON.parse(JSON.stringify(pkgContent));
+}
+
+/**
+* Get a specific value from the package.json file, or a default if the
+* value is not set.
+*
+* @param {String} name The name of the value.
+* @param {String} defaultValue A default value to return.
+* @returns {{}} A clone of the referenced value.
+*/
+function getValue (name, defaultValue) {
+if (pkgContent.hasOwnProperty(name)) {
+  return JSON.parse(JSON.stringify(pkgContent[name]));
+}
+return defaultValue || undefined;
+}
+
+/**
+ * Create a specific NPM script command, invoked by 'npm run <name>' or 'npm <name>'.
+ *
+ * @param {String} name The name of the script.
+ * @param {String} command The command to invoke.
+ * @returns {void}
+ */
+function addCommand (name, command) {
+
+  // sanity check, does 'scripts' exist?
+  if (!pkgContent.hasOwnProperty('scripts')) {
+    pkgContent.scripts = {};
   }
 
-  /**
-   * Initialize this builder from a JSON string.
-   *
-   * @param {String} pkgString The package string content.
-   * @returns {void}
-   */
-  function readPackage (pkgString) {
-    pkgContent = JSON.parse(pkgString);
-  }
+  // Save/Override the command.
+  pkgContent.scripts[name] = command;
+}
 
-  /**
-   * Write the package content to a JSON string.
-   *
-   * @returns {String} The JSON content of the package, as a string.
-   */
-  function writePackage () {
-    // Clone the package content so we don't destroy what's in memory...
-    var newContent = JSON.parse(JSON.stringify(pkgContent));
-
-    // Synchronize all the dependencies.
-    newContent.peerDependencies = synchronizeDependencies(newContent.peerDependencies);
-    newContent.dependencies = synchronizeDependencies(newContent.dependencies);
-    newContent.devDependencies = synchronizeDependencies(newContent.devDependencies);
-    return JSON.stringify(newContent, null, 2);
-  }
-
-  /**
-   * Add libraries to the package dependencies.
-   *
-   * @param {[]|String} libraryNames A list of all libraries to add to the dependencies.
-   * @param {String} type The type of dependency.
-   * @returns {void}
-   */
-  function addDependencies (libraryNames, type) {
-    // Default the type.
-    type = type || 'dependencies';
-
-    // Valuecheck type.
-    if (['devDependencies', 'peerDependencies', 'dependencies'].indexOf(type) === -1) {
-      return;
-    }
-
-    // Default the array.
-    if (!Array.isArray(libraryNames)) {
-      libraryNames = [libraryNames];
-    }
-
-    // Make sure the property exists.
-    if (!pkgContent.hasOwnProperty(type)) {
-      pkgContent[type] = {};
-    }
-
-    // Add the dependency
-    libraryNames.forEach(function (library) {
-      var version = dependencies.read(library);
-      if (version && !pkgContent[type].hasOwnProperty(library)) {
-        pkgContent[type][library] = version;
-      }
-    });
-  }
-
-  /**
-   * Set values on the current package.
-   *
-   * @param {{}} values A map of values.
-   * @returns {void}
-   */
-  function setValues (values) {
-    for (var key in values) {
-      // Filter out things from prototype.
-      if (values.hasOwnProperty(key)) {
-        pkgContent[key] = values[key];
-      }
-    }
-  }
-
-  /**
-   * Get the values of the current package.
-   *
-   * @returns {{}} A cloned map of the values.
-   */
-  function getValues () {
-    return JSON.parse(JSON.stringify(pkgContent));
-  }
-
-  /**
-   * Get a specific value from the package.json file, or a default if the
-   * value is not set.
-   *
-   * @param {String} name The name of the value.
-   * @param {String} defaultValue A default value to return.
-   * @returns {{}} A clone of the referenced value.
-   */
-  function getValue (name, defaultValue) {
-    if (pkgContent.hasOwnProperty(name)) {
-      return JSON.parse(JSON.stringify(pkgContent[name]));
-    }
-    return defaultValue || undefined;
-  }
-
-  /**
-   * Create a specific NPM script command, invoked by 'npm run <name>' or 'npm <name>'.
-   *
-   * @param {String} name The name of the script.
-   * @param {String} command The command to invoke.
-   * @returns {void}
-   */
-  function addCommand (name, command) {
-
-    // sanity check, does 'scripts' exist?
-    if (!pkgContent.hasOwnProperty('scripts')) {
-      pkgContent.scripts = {};
-    }
-
-    // Save/Override the command.
-    pkgContent.scripts[name] = command;
-  }
-
-  module.exports = {
-    fromJSON: readPackage,
-    toJSON: writePackage,
-    setValues: setValues,
-    getValues: getValues,
-    getValue: getValue,
-    addDependencies: addDependencies,
-    addCommand: addCommand
-  };
-})();
+module.exports = {
+  fromJSON: readPackage,
+  toJSON: writePackage,
+  setValues: setValues,
+  getValues: getValues,
+  getValue: getValue,
+  addDependencies: addDependencies,
+  addCommand: addCommand
+};
