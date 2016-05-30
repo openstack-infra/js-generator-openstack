eb641605a684d8b8d43fc2e86d41f429ec5078e7
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-05-30 23:17:46 +0800

    (WIP) add jsdoc3 to generate api documentation
    run `gulp` and serve the document from `docs/gen`
    
    Change-Id: Ica8202a10b1d2c2a35453637f600501a71e412b7
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

--- generators/app/lib/pkg_builder.js
+++ generators/app/lib/pkg_builder.js
@@ -1,71 +1,69 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var pkgContent = {};
+var pkgContent = {};
 
-  /**
-   * Initialize this builder from a JSON string.
-   *
-   * @param {String} pkgString The package string content.
-   * @returns {void}
-   */
-  function readPackage (pkgString) {
-    pkgContent = JSON.parse(pkgString);
-  }
+/**
+ * Initialize this builder from a JSON string.
+ *
+ * @param {String} pkgString The package string content.
+ * @returns {void}
+ */
+function readPackage (pkgString) {
+  pkgContent = JSON.parse(pkgString);
+}
 
-  /**
-   * Write the package content to a JSON string.
-   *
-   * @returns {String} The JSON content of the package, as a string.
-   */
-  function writePackage () {
-    return JSON.stringify(pkgContent, null, 2);
-  }
+/**
+ * Write the package content to a JSON string.
+ *
+ * @returns {String} The JSON content of the package, as a string.
+ */
+function writePackage () {
+  return JSON.stringify(pkgContent, null, 2);
+}
 
-  /**
-   * Set values on the current package.
-   *
-   * @param {{}} values A map of values.
-   * @returns {void}
-   */
-  function setValues (values) {
-    for (var key in values) {
-      // Filter out things from prototype.
-      if (values.hasOwnProperty(key)) {
-        pkgContent[key] = values[key];
-      }
+/**
+ * Set values on the current package.
+ *
+ * @param {{}} values A map of values.
+ * @returns {void}
+ */
+function setValues (values) {
+  for (var key in values) {
+    // Filter out things from prototype.
+    if (values.hasOwnProperty(key)) {
+      pkgContent[key] = values[key];
     }
   }
+}
 
-  /**
-   * Get the values of the current package.
-   *
-   * @returns {{}} A cloned map of the values.
-   */
-  function getValues () {
-    return JSON.parse(JSON.stringify(pkgContent));
+/**
+ * Get the values of the current package.
+ *
+ * @returns {{}} A cloned map of the values.
+ */
+function getValues () {
+  return JSON.parse(JSON.stringify(pkgContent));
+}
+
+/**
+ * Get a specific value from the package.json file, or a default if the
+ * value is not set.
+ *
+ * @param {String} name The name of the value.
+ * @param {String} defaultValue A default value to return.
+ * @returns {{}} A clone of the referenced value.
+ */
+function getValue (name, defaultValue) {
+  if (pkgContent.hasOwnProperty(name)) {
+    return JSON.parse(JSON.stringify(pkgContent[name]));
   }
+  return defaultValue || undefined;
+}
 
-  /**
-   * Get a specific value from the package.json file, or a default if the
-   * value is not set.
-   *
-   * @param {String} name The name of the value.
-   * @param {String} defaultValue A default value to return.
-   * @returns {{}} A clone of the referenced value.
-   */
-  function getValue (name, defaultValue) {
-    if (pkgContent.hasOwnProperty(name)) {
-      return JSON.parse(JSON.stringify(pkgContent[name]));
-    }
-    return defaultValue || undefined;
-  }
-
-  module.exports = {
-    fromJSON: readPackage,
-    toJSON: writePackage,
-    setValues: setValues,
-    getValues: getValues,
-    getValue: getValue
-  };
-})();
+module.exports = {
+  fromJSON: readPackage,
+  toJSON: writePackage,
+  setValues: setValues,
+  getValues: getValues,
+  getValue: getValue
+};
